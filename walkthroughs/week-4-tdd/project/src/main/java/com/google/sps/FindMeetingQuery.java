4af5ea65dffc6f34d38bd33a3d99b635b95b60a8
Yuxuan Zhang <yzhang2@sps-program.com>
2020-07-19 11:37:37 +0800

    modify FindMeetingQuery.java to pass the tests

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,70 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+       
+        // Initialization
+        ArrayList<TimeRange> TimeSegList = new ArrayList<TimeRange>();
+        ArrayList<TimeRange> Answer = new ArrayList<TimeRange>();
+        
+        // Find all invalid time ranges
+        for (Event nowEvent: events) {
+            
+            Set<String> eventAttendees = nowEvent.getAttendees();
+            Collection<String> requestAttendees = request.getAttendees();
+            boolean flag = false;
+
+            for (String nowAtt: requestAttendees) {
+                if (eventAttendees.contains(nowAtt)) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag == true) {
+                TimeSegList.add(nowEvent.getWhen());
+            }
+
+            /* Here the use of retainAll seems to be wrong... but why?
+            eventAttendees.retainAll(request.getAttendees());
+            if (eventAttendees.size() > 0) {
+                TimeSegList.add(nowEvent.getWhen());
+            }
+            */
+        }
+
+        // sort the time ranges by their start time
+        Collections.sort(TimeSegList, TimeRange.ORDER_BY_START);
+
+        // R: point to the latest available time
+        int R = TimeRange.START_OF_DAY;
+        for (TimeRange nowSeg: TimeSegList) {
+            
+            // if there's a valid time range, add into Answer
+            if (nowSeg.start() > R && nowSeg.start() - R >= request.getDuration()) {
+                Answer.add(TimeRange.fromStartEnd(R, nowSeg.start(), false));
+            }
+
+            // update R
+            if (nowSeg.end() > R) {
+                R = nowSeg.end();
+            }
+        }
+
+        // add the last time range
+        if (TimeRange.END_OF_DAY - R + 1 >= request.getDuration()) {
+            Answer.add(TimeRange.fromStartEnd(R, TimeRange.END_OF_DAY, true));
+        }
+
+        return Answer;
+    }
 }
